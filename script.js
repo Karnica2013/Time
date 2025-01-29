1ea8b402b899ab21950a296da67a6cce80e06f65
Karnica2013 <karnicasrir1@educbe.ca>
2025-01-28 18:30:21 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+setInterval(function(){
+    let d = new Date();
+    document.getElementById("hrs").innerHTML = d.getHours();
+    document.getElementById("mins").innerHTML = d.getMinutes();
+    document.getElementById("secs").innerHTML = d.getSeconds();
+    document.getElementById("date").innerHTML =
+        d.getDay() + "    " + d.getDate() + "-" + d.getMonth() + 1 + "-" + d.getFullYear()
+    
+},1000)
